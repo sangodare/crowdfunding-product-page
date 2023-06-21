7a2b9bfcde26ec2e53494fcba0b5c9bfc8dbca16
sangodare <sangodare.toyeeb@gmail.com>
2023-06-22 00:28:17 +0100

    Added functionality and readme file

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Crowdfunding product page solution
+
+This is a solution to the [Crowdfunding product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/crowdfunding-product-page-7uvcZe7ZR).  
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [What I learned](#what-i-learned)
+- [Continued development](#continued-development)
+- [Useful resources](#useful-resources)
+- [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout depending on their device's screen size
+- See hover states for interactive elements
+- Make a selection of which pledge to make
+- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
+- See the number of total backers increment by one after confirming a pledge
+- Toggle whether or not the product is bookmarked
+
+### Screenshot
+
+![](images/screenshot.png)
+
+### Links
+
+- Solution URL: [Github](https://github.com/sangodare/crowdfunding-product-page)
+- Live Site URL: [Netlify link](https://crowdfundingprdpagebytoyeeb.netlify.app/)
+
+### Built with HTML CSS and JavaScript
+
+The solution to the challenge was built with HTML, CSS and JavaScript. I made sure the solution i built is a close match  to the design provided by Frontend Mentor. I made use of semantic HTML5 markup such as nav, button, header, section and so on. flexbox and CSS GRID came in handy for the layout. An additonal feature was added, the data on the progress made is stored in the local storage so that when a user reloads or revisits the page, any update to the progress made will take into account the data from the previous page visits. 
+
+### What I learned
+
+I learnt how to create accessible mobile menu using the appropriate aria-labels such as aria-controls and aria-expanded, assign roles to elements e.g the element that controls the menu has role 'button' while the element that contains the menu has role 'menu'.
+
+Also learnt about positoning elements in CSS, how it affects the layout e.g using 'position: absolute' on a transformed element can cause overflow which can lead to horizontal scrolling.
+
+Also learnt that background images in CSS are not accessible.
+
+Moving forward, accessibility will be at the forefront of any project i build, my goal is to use more semantic HTML and  learn more about accessibility, so as to ensure anything i build is accessible.
+
+## Author
+
+Connect with me on Twitter [@Iamtoyeeb](https://www.twitter.com/Iamtoyeeb).
+
--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 'use strict';
+
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 const overlay = document.querySelector('.overlay');
 const nav = document.querySelector('.nav');
+const backProjectBtn = document.querySelector('.back-project');
+const rewardBtns = document.querySelectorAll('.reward-btn');
+const selectionModal = document.querySelector('.selection-modal');
+const numInputs = document.querySelectorAll('input[type=number]');
+const closeModal = document.querySelector('.close-modal');
+const checkboxInputs = document.querySelectorAll('input[type=checkbox]');
+const continueBtns = document.querySelectorAll('.continue-btn');
+const thanksModal = document.querySelector('.thanks-modal');
+const gotItBtn = document.querySelector('.got-it-btn');
+const progress = document.querySelector('.progress');
+const amountRaised = document.querySelector('.amount');
+const backers = document.getElementById('num-of-backers');
+const progressBar = document.querySelector('.progress-bar-filled');
+const bookmarkContainer = document.querySelector('.bookmark');
+const bookmarkSvg = document.querySelector('.bookmark-svg');
+const bookmarkButton = document.querySelector('.bookmark-btn');
+
+let bookmarked;
+const dataObj = {};
+
+
+//Functions
 
 //overlay toggle
 function displayOverlay() {
-    overlay.classList.remove('hidden');
+    overlay.classList.remove('overlay-hidden');
+    document.body.style.overflow = 'hidden';
 };
 
 function hideOverlay() {
-    overlay.classList.add('hidden');
+    overlay.classList.add('overlay-hidden');
+    document.body.style.overflow = 'scroll';
 };
 
 //Mobile menu toggle
@@ -19,15 +44,15 @@ function toggleMenu() {
     if(visibility === 'false') {
         mobileNavToggle.setAttribute('aria-expanded', true);
         nav.setAttribute('data-visible', true);
-        displayOverlay()
+        closeSelectionModal();
+        hideThanksModal();
+        displayOverlay();
     } else {
         mobileNavToggle.setAttribute('aria-expanded', false);
         nav.setAttribute('data-visible', false);
         hideOverlay();
     }
 };
-
-mobileNavToggle.addEventListener('click', toggleMenu);
 
 //menu link fade, when a menu link is hovered other menu links fade
 function linkHighlighter(e) {
@@ -40,5 +65,215 @@ function linkHighlighter(e) {
     }
 };
 
+function bookmark() {
+    bookmarkSvg.classList.add('bookmarked');
+    bookmarkButton.textContent = 'Bookmarked';
+    bookmarkButton.classList.add('bookmarked-btn');
+    bookmarked = true;
+};
+
+function unbookmark() {
+    bookmarkSvg.classList.remove('bookmarked');
+    bookmarkButton.textContent = 'Bookmark';
+    bookmarkButton.classList.remove('bookmarked-btn');
+    bookmarked = false;
+}
+
+
+function bookmarkToggle() {
+    bookmarkContainer.classList.toggle('toggler');
+
+    if (bookmarkContainer.classList.contains('toggler'))  bookmark();
+
+    if(!bookmarkContainer.classList.contains('toggler')) unbookmark();
+
+    dataObj.bookmarked = bookmarked;
+
+    localStorage.setItem('data', JSON.stringify(dataObj));
+       
+};
+
+function displaySelectionModal() {
+    displayOverlay();
+    selectionModal.classList.remove('hidden');
+    
+};
+
+function closeSelectionModal() {
+    hideOverlay();
+    selectionModal.classList.add('hidden');
+    resetPledge();
+};
+
+function displayStand(element) {
+    element.classList.add('modal-stand--active');
+    element.querySelector('.pledging').classList.remove('hidden');
+    element.querySelector('.stand-content').classList.remove('border');
+};
+
+function hideStand(element) {
+    element.classList.remove('modal-stand--active');
+    element.querySelector('.pledging').classList.add('hidden');
+    element.querySelector('.stand-content').classList.add('border');
+};
+
+function resetPledge() {
+    checkboxInputs.forEach(checkbox => {
+        const checkboxParentEl = checkbox.closest(`.modal-stand--${checkbox.dataset.input}`);
+        checkbox.checked = false;
+        hideStand(checkboxParentEl);
+    });
+};
+
+function displayRewardSelected(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('out-of-stock-bg')) return;
+  
+    const id = e.target.getAttribute('href');
+    const dataNum = e.target.dataset.reward;
+    const reward = document.querySelector(id);
+
+    displaySelectionModal();
+    reward.scrollIntoView({behavior: 'smooth'});
+    reward.querySelector(`.input--${dataNum}`).checked = true;
+    displayStand(reward);
+};
+
+function selectPledge(e) {
+    const clicked = e.target;
+    const allCheckboxInputs = clicked.closest('.selection-modal').querySelectorAll('.input');
+    const inputParentEl = clicked.closest(`.modal-stand--${clicked.dataset.input}`);
+
+    allCheckboxInputs.forEach(checkbox => {
+        const checkboxParentEl = checkbox.closest(`.modal-stand--${checkbox.dataset.input}`);
+
+        if(clicked.classList.contains('input--4')) return;
+    
+        if(checkbox !== clicked) checkbox.checked = false;
+
+        if(checkbox.checked === false) hideStand(checkboxParentEl);
+        
+        if(clicked.checked === true) displayStand(inputParentEl);
+    
+    });
+
+};
+
+//to restrict amount entered as pledge
+function restrictNumInput() {
+    this.value = this.valueAsNumber;
+    if(this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);
+};
+
+function updateBackers() {
+    let currentBackers = backers.textContent.replaceAll(',', '');
+    const numOfBackers = ++currentBackers;
+    const formattedNum = new Intl.NumberFormat(navigator.language, {style: 'decimal'}).format(numOfBackers);
+
+    dataObj.formattedNumOfBackers = formattedNum,
+    
+    localStorage.setItem('data', JSON.stringify(dataObj));
+    backers.textContent = `${formattedNum}`;
+};
+
+function updateAmountRaised(input) {
+    let currentAmountRaised = amountRaised.textContent.replaceAll(',', '');
+    const inputAmount = +input.value
+    const updatedAmount =  +currentAmountRaised + inputAmount;
+    const formattedAmount = new Intl.NumberFormat(navigator.language, {style: 'decimal'}).format(updatedAmount);
+
+    dataObj.formattedAmountRaised = formattedAmount;
+    localStorage.setItem('data', JSON.stringify(dataObj));
+    amountRaised.textContent = `${formattedAmount}`;
+};
+
+function updateProgressBar() {
+    const data = JSON.parse(localStorage.getItem('data'));
+    const figureRaised = data?.formattedAmountRaised || amountRaised.textContent.replaceAll(',', '');
+    const percent = (+figureRaised / 100000) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+};
+
+function getData() {
+    const data = JSON.parse(localStorage.getItem('data'));
+    backers.textContent = data?.formattedNumOfBackers || backers.textContent;
+    amountRaised.textContent = data?.formattedAmountRaised || amountRaised.textContent;
+    updateProgressBar();
+
+    if(data?.bookmarked === true) {
+        bookmarkContainer.classList.add('toggler');
+        bookmark();
+    }
+};
+
+function updateProgress(e) {
+    const parentEl = e.target.closest('.pledging');
+    const input = document.querySelector(`.pledge-input--${e.target.dataset.tab}`);
+
+    if(input?.value == null) {
+        closeSelectionModal();
+        displayThanksModal();
+        updateBackers();
+    };
+
+    if(input?.value != null) {
+        if(input.value === '' || +input.value < +input.min || +input.value > +input.max) {
+            const html = `<p class="margin-top error-msg">Your pledge should be between ${input.min} and ${input.max}</p>`
+    
+            if(parentEl.querySelector('.error-msg') != null) return;
+            parentEl.insertAdjacentHTML('beforeend', html);
+    
+            setTimeout(() => parentEl.querySelector('p').remove(), 3000);  
+        };
+
+        updateBackers();
+        updateAmountRaised(input);
+        updateProgressBar();
+        closeSelectionModal();
+        displayThanksModal();
+    };
+    
+};
+
+function displayThanksModal() {
+    displayOverlay();
+    thanksModal.classList.remove('hidden');
+};
+
+function hideThanksModal() {
+    hideOverlay();
+    thanksModal.classList.add('hidden');
+};
+
+function scrollToProgressSection() {
+    progress.scrollIntoView({behavior: 'smooth'});
+}
+
+
+//Event listeners
+mobileNavToggle.addEventListener('click', toggleMenu);
+
 nav.addEventListener('mouseover', linkHighlighter.bind(0.5));
+
 nav.addEventListener('mouseout', linkHighlighter.bind(1));
+
+bookmarkContainer.addEventListener('click', bookmarkToggle);
+
+backProjectBtn.addEventListener('click', displaySelectionModal);
+
+rewardBtns.forEach(rewardBtn => rewardBtn.addEventListener('click', displayRewardSelected));
+
+checkboxInputs.forEach(checkbox => checkbox.addEventListener('click', selectPledge));
+
+numInputs.forEach(numInput => numInput.addEventListener('input', restrictNumInput));
+
+closeModal.addEventListener('click', closeSelectionModal);
+
+continueBtns.forEach(continueBtn => continueBtn.addEventListener('click', updateProgress));
+
+gotItBtn.addEventListener('click', () => {
+    hideThanksModal();
+    scrollToProgressSection();
+});
+
+document.addEventListener('DOMContentLoaded', getData);
